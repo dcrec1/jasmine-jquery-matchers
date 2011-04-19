dc470863b8ef1599b1b964713d34e4c66612bdf2
Diego Carrion <dc.rec1@gmail.com>
2011-04-19 12:41:09 -0300

    first commit

---
+++ matchers.js
@@ -0,0 +1,39 @@
+beforeEach(function() {
+  this.addMatchers({
+    toBeACalendar: function() {
+      return this.actual.hasClass("hasDatepicker");             
+    },
+
+    toBeClosed: function() {
+      return !this.actual.dialog("isOpen");
+    },
+
+    toBeADialog: function() {
+      return this.actual.parent().hasClass("ui-dialog");             
+    },
+
+    toBeASlider: function() {
+      return this.actual.hasClass("ui-slider");             
+    },
+
+    toBeATextInput: function() {
+      return this.actual.is(":text");
+    },
+
+    toBeDefaultPrevented: function() {
+      return this.actual.isDefaultPrevented();                    
+    },
+
+    toBeHidden: function() {
+      return this.actual.is(":hidden");             
+    },
+
+    toBeVisible: function() {
+      return this.actual.is(":visible");             
+    },
+
+    toHaveClass: function(className) {
+      return this.actual.hasClass(className);             
+    }
+  });
+});
